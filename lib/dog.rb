071a863d2e15ca19258986a9d697a247592c8e8c
Priscilla Leyva <pleyva.co89@gmail.com>
2020-09-03 04:11:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 
-  def name=(dog_name)
+  def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
 
@@ -12,4 +12,4 @@ end
 lassie = Dog.new
 lassie.name = "Lassie"
 
-puts lassie.name 
+puts lassie.name
